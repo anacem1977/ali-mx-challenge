984ca58974fbc68b7a7d04a3ced99c97b87b1ad8
anace-m <maldonadoanaceciliac@johndeere.com>
2021-04-06 17:40:18 -0500

    created components, header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import { Route, Link } from "react-router-dom";
 import './App.css';
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faClipboard} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faClipboard);
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +23,18 @@ class App extends Component {
   render () {
   return (
     <div className="App">
-        <h1>welcome</h1>
+      <header>
+        <Container>
+          <Row>
+            <Col sm={8}>
+              <h1>Título de Formulario</h1>
+            </Col>
+            <Col sm={4}>
+              <FontAwesomeIcon icon="clipboard" size="2x"></FontAwesomeIcon>
+            </Col>
+          </Row>
+        </Container>
+      </header>
     </div>
   );
 }
